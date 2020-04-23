e580759e61205190477fb71577da6c908ce7658d
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-23 15:45:54 +0700

    Update iglimit.py

--- iglimit.py
+++ iglimit.py
@@ -1,16 +1,21 @@
 import time
-import os
+import os, sys, random
 
-green=33[32;1m
-red=33[31;1m
-
-print (green+"woy pantek")
+green=\33[32;1m
+red=\33[31;1m
+print (“\33[32;1m”)
+print (“woy pantek")
 time.sleep(3)
-print (red+"i am sorry")
+print (“i am sorry")
 time.sleep(2)
 print ("tools sedang bermasalah")
 time.sleep(2)
 print ("dan sedang di perbaiki oleh admin")
 time.sleep(2)
-print (green+"subscribe aja ya nanti ku perbaiki toolsnya")
+print (“subscribe aja ya lagi ku perbaiki toolsnya")
 time.sleep(2)
+os.system(‘xdg-open https://www.youtube.com/channel/UC1rlhLpR7f1ehlvx7IRgEvg’)
+print (“like dulu ajik”)
+time.sleep (3)
+os.system(‘xdg-open https://www.youtube.com/channel/UC1rlhLpR7f1ehlvx7IRgEvg‘)
+print (“gua gans thanks”)
